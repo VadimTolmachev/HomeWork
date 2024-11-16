9b9fb0259712a9d154a549fdefe964178adaf368
Vadim <tolmachvv@yandex.ru>
2024-11-16 13:04:22 +0700

    module_1_4

---
+++ HomeWork/modul_1_4.py
@@ -0,0 +1,5 @@
+number_of_completed_DZ = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_of_one_task = number_of_hours_spent / number_of_completed_DZ
+print('Курс:', course_name, ', затрачено часов: ',number_of_completed_DZ, ' среднее время выполнения ', time_of_one_task, 'часов')
