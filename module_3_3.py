9e9ba01cbd6610f8ed4ecdbe2cdbaede3209cd03
VadimTolmachev <tolmachvv@yandex.ru>
2024-12-04 20:21:53 +0700

    format pip8

--- module_3_3.py
+++ module_3_3.py
@@ -1,15 +1,16 @@
-def print_params(a = 1, b = 'строка', c = True):
+def print_params(a=1, b='строка', c=True):
     print(a, b, c)
+
 
 print_params()
 print_params(b=25)
-print_params(c = [1, 2, 3])
+print_params(c=[1, 2, 3])
 
-values_list = [4, 5 ,9]
+values_list = [4, 5, 9]
 print_params(*values_list)
 
-values_list = [3.14 , 'Пример!']
+values_list = [3.14, 'Пример!']
 print_params(*values_list, 'Стоп')
 
-values_dict = {'a':2, 'b':3, 'c':4}
+values_dict = {'a': 2, 'b': 3, 'c': 4}
 print_params(**values_dict)
