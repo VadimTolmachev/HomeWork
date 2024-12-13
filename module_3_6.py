e6cef9c2d951dff42a70022b0fbd59cd6c6f7b27
VadimTolmachev <tolmachvv@yandex.ru>
2024-12-13 17:40:42 +0700

    Edit a recursion function

--- module_3_6.py
+++ module_3_6.py
@@ -5,30 +5,33 @@ data_structure = [
     "Hello",
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-sum = 0
+sum_ = 0
+len_str = 0
 
 
-# Функция сложения чисел целых и с точкой применяется рекурсия.
+# Функция сложения чисел и длинны строк.
 def new_sum(*args):
-    global sum
+    global sum_, len_str
     values = args[0]
     # если тип значения int или float - суммируем
     if isinstance(values, int) or isinstance(values, float):
-        sum += values
+        sum_ += values
         return
     # если значение список(list), кортеж(tuple) или множество(set) - рекурсия
     elif isinstance(values, list) or isinstance(values, tuple) or isinstance(values, set):
         for i in values:
             new_sum(i)
-    # если словарь(dict) - рекурсия только по значению
+    # если словарь(dict) - рекурсия по значению и по ключам
     elif isinstance(values, dict):
         for key, val in values.items():
             new_sum(val)
+            new_sum(key)
+    elif isinstance(values, str):
+        # print(f'длина строки - {len(values)} - {values}')
+        len_str += len(values)
     else:
         return
 
+new_sum(data_structure)
 
-for i in data_structure:
-    new_sum(i)
-
-print(sum)
+print(f'Сумма чисел - {sum_}, сумма строк - {len_str}, всего - {sum_ + len_str}')
