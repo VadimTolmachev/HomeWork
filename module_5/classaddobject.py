728bff9960a795fa699e14e3f695e14b95745f91
VadimTolmachev <tolmachvv@yandex.ru>
2024-12-26 17:35:16 +0700

    Class

---
+++ module_5/classaddobject.py
@@ -0,0 +1,31 @@
+class Human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.say_info()
+
+    def say_info(self):
+        print(f'Привет, меня зовут {self.name}, мне {self.age} лет.')
+
+    def birthday(self):
+        self.age += 1
+        print(f'У меня сегодня день рождения, и мне теперь {self.age} лет.')
+
+    def __del__(self):
+        print(f'{self.name} ушел.')
+
+    def __len__(self):
+        return self.age
+
+den = Human('Денис', 22)
+macks = Human('Максим', 22)
+
+den.age = 23
+print(len(den))
+del den
+# print(den.name, den.age)
+# print(macks.name, macks.age)
+#den.say_info()
+# macks.say_info()
+macks.birthday()
+macks.say_info()
